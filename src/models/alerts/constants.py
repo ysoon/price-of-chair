194683e28d2a1280ab683ce97d076c9ad37026a1
YSLIM <ysoon.biz@gmail.com>
2018-01-23 11:51:23 +0800

    Adding required Heroku files

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,8 @@
 __author__ = 'jslvtr'
 
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+URL = "https://api.mailgun.net/v3/sandbox3ed1c9a9adfc4540a2a11897d0d1b65c.mailgun.org/messages"
+API_KEY = "key-e7c2563ce6fa374303c3277d4a4206f5"
+FROM = "postmaster@sandbox3ed1c9a9adfc4540a2a11897d0d1b65c.mailgun.org"
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
